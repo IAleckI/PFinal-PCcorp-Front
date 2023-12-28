182956dea84be44473c83d5a4bd387822e5d6ddb
IAleckI <healsava12@gmail.com>
2023-12-28 14:21:24 -0500

    navbar obsoleta se remueve

--- Client/src/components/Index.js
+++ Client/src/components/Index.js
@@ -1,6 +1,5 @@
 import Button from "./Button/button";
 import SearchBar from "./SearchBar/SearchBar";
 import Carousel from "./Carrusel/carouselLanding";
-import NavBar from "./NavBar/Navbar";
 import Footer from "./Footer/footer";
-export { Button, SearchBar, Carousel, NavBar, Footer };
+export { Button, SearchBar, Carousel, Footer };
