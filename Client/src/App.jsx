35c2672015ace1fe3fbe8b8d508f3297fda75727
IAleckI <healsava12@gmail.com>
2023-12-28 13:36:33 -0500

    Footer base
    se crea footer con logo a github

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,13 +4,11 @@ import { Home, Login, Detail } from "./Views/Index";
 
 function App() {
   return (
-    <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route pa th="/detail" element={<Detail />} />
       </Routes>
-    </div>
   );
 }
 
--- Client/src/Views/Home/Home.jsx
+++ Client/src/Views/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Carousel, NavBar } from '../../components/Index'
+import { Carousel, NavBar, Footer } from '../../components/Index'
 
 const Home = () => {
   return (
     <div>
       <NavBar />
       <Carousel />
+      <Footer />
     </div>
   )
 }
