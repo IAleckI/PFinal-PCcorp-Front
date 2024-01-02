6ff6ed84c24f8a4aaa13797c0513adee37a418aa
TSantiagoIdalgo <tomasidalgo51@gmail.com>
2024-01-02 00:56:33 -0300

    navbar

--- src/components/Index.js
+++ src/components/Index.js
@@ -2,7 +2,7 @@ import { Button} from "./Button/button";
 import SearchBar from "./SearchBar/SearchBar";
 import Carousel from "./Carrusel/carouselLanding";
 import Footer from "./Footer/footer";
-import NavBar from "./NavBar/navBar";
+import NavBar from "./NavBar/navbar";
 import LoginComponent from "./Login/loginComponent";
 import Banner from "./banner/banner";
 import CardList from "./cardList/carList";
