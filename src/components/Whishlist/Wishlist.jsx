54fd6a4e29bc5124bc06ebc043fa0602915d3532
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-01-03 16:40:57 -0300

    Wishilist

---
+++ src/components/Whishlist/Wishlist.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavBar, Footer } from '../Index'
+
+const Wishlist = () => {
+  return (
+    <div>
+        <NavBar/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Wishlist
