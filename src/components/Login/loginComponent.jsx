942f6240f5a189f42b947ff2752ed55d32bd3891
RHolguinCaruso <robert23.holguin06@gmail.com>
2024-01-04 14:44:12 -0500

    Creacion de componente de creacion
    El componente no esta completado pero se encuentra maquetado, falta implementarle el middleware de validaciones y luego el submit a la base de datos

--- src/components/Login/loginComponent.jsx
+++ src/components/Login/loginComponent.jsx
@@ -1,25 +1,23 @@
-import Style from './loginComponent.module.css'
-import { useGoogle } from '../../utils/hooks/network/google/useGoogle'
-import { useFacebook } from '../../utils/hooks/network/facebook/useFacebook'
-import { Link } from 'react-router-dom'
-import LoginData from './loginData/loginData'
-import GoogleLogin from 'react-google-login'
-import ReactFacebookLogin from 'react-facebook-login'
+import Style from "./loginComponent.module.css";
+import { useGoogle } from "../../utils/hooks/network/google/useGoogle";
+import { useFacebook } from "../../utils/hooks/network/facebook/useFacebook";
+import { Link } from "react-router-dom";
+import LoginData from "./loginData/loginData";
+import GoogleLogin from "react-google-login";
+import ReactFacebookLogin from "react-facebook-login";
 
 export default function LoginComponent() {
-  const clientId = useGoogle()
-  const { fbId, success, error } = useFacebook()
+  const clientId = useGoogle();
+  const { fbId, success, error } = useFacebook();
 
   return (
-
-
-    <div className={styles.box}>
-      <form className={styles.formContainer} onSubmit={handleFormSubmit}>
-        <label className={styles.label} htmlFor="email">
+    <div className={Style.box}>
+      <form className={Style.formContainer} onSubmit={handleFormSubmit}>
+        <label className={Style.label} htmlFor="email">
           Email
         </label>
         <input
-          className={styles.input}
+          className={Style.input}
           type="email"
           id="email"
           name="email"
@@ -27,11 +25,11 @@ export default function LoginComponent() {
           onChange={handleEmailChange}
         />
 
-        <label className={styles.label} htmlFor="password">
+        <label className={Style.label} htmlFor="password">
           Password
         </label>
         <input
-          className={styles.input}
+          className={Style.input}
           type="password"
           id="password"
           name="password"
@@ -39,39 +37,46 @@ export default function LoginComponent() {
           onChange={handlePasswordChange}
         />
 
-        <button type="submit" className={styles.button}>
+        <button type="submit" className={Style.button}>
           Login
         </button>
-        <div className={styles.google}>
-        <GoogleLogin
-        
-        clientId={clientID}
-        buttonText="Login with Google"
-        onSuccess={onSuccess}
-        onFailure={onFailure}
-        cookiePolicy={'single_host_origin'}
-      /></div>
-       <div className={styles.facebook}>
+        <div className={Style.google}>
+          <GoogleLogin
+            clientId={clientID}
+            buttonText="Login with Google"
+            onSuccess={onSuccess}
+            onFailure={onFailure}
+            cookiePolicy={"single_host_origin"}
+          />
+        </div>
+        <div className={Style.facebook}>
           <FacebookLogin
             appId={facebookAppID}
             autoLoad={false}
             fields="name,email,picture"
             callback={facebookResponse}
             render={(renderProps) => (
-              <button onClick={renderProps.onClick} className={styles.facebookButton}>
+              <button
+                onClick={renderProps.onClick}
+                className={Style.facebookButton}
+              >
                 Login with Facebook
               </button>
             )}
           />
-          <div className={Style.login_middle}/>
-          <LoginData/>
+          <div className={Style.login_middle} />
+          <LoginData />
         </div>
-      
-       <p className={styles.signin}>¿You dont have an account yet? <a href="/register"> Sign In!</a> </p> 
-       
-      <button className={styles.button} onClick={() => window.history.back()}>go back</button>
+
+        <p className={Style.signin}>
+          ¿You dont have an account yet? <a href="/register"> Sign In!</a>{" "}
+        </p>
+
+        <button className={Style.button} onClick={() => window.history.back()}>
+          go back
+        </button>
       </form>
-    <LogoutButton className={styles.button}/>
+      <LogoutButton className={Style.button} />
     </div>
-  )
+  );
 }
