70884ea1fe2512b482dd208a90578f0c914e8fb4
IAleckI <healsava12@gmail.com>
2024-01-04 13:17:00 -0500

    se modifica view de about as se corrigen rutas

--- src/Views/AboutUs/AboutUs.jsx
+++ src/Views/AboutUs/AboutUs.jsx
@@ -1,19 +1,10 @@
-// AboutUs.js
-import React from 'react';
-
+import { NavBar, Footer } from "../../components/Index";
 const AboutUs = () => {
   return (
     <div>
-      <h1>Quiénes somos</h1>
-      <p>
-        Bienvenido a nuestra aplicación. Somos un equipo apasionado comprometido
-        a proporcionar soluciones increíbles a nuestros usuarios.
-      </p>
-      <p>
-        En nuestra misión de excelencia, nos esforzamos por ofrecer productos y
-        servicios de alta calidad que satisfagan las necesidades de nuestros clientes.
-      </p>
-      {/* Se puede agregar más contenido según sea necesario */}
+      <NavBar />
+      /* ---------------- aqui debe ir la renderizacion del aboutus --------------- */
+      <Footer />
     </div>
   );
 };
