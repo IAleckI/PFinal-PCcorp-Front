6d039076058a828dd1b2f00ad6a586307c02789f
RHolguinCaruso <robert23.holguin06@gmail.com>
2024-01-05 14:16:20 -0500

    Create CreateFormComponent

--- src/components/FormCreateComponent/FormCreateComponent.jsx
+++ src/components/FormCreateComponent/FormCreateComponent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import Style from "./FormCreateComponent.module.css";
-// import { createProduct } from "../../utils/hooks/validates/productSchema";
+import Input from "../input/input";
 
 const FormCreateComponent = () => {
   const {
@@ -14,36 +14,83 @@ const FormCreateComponent = () => {
   const onSubmit = (data) => {
     console.log(data);
 
-
-
     console.log(watch("example"));
   };
 
   return (
-    <div>
+    <div className={Style.container}>
       <h1 className={Style.title}>FormCreateComponent</h1>
 
       <form className={Style.form}>
-        <label htmlFor="">Url imagen</label>
-        <input type="text" {...register("image")} />
+        <Input
+          info={register("image")}
+          error={errors.image?.message}
+          name="image"
+          type={"text"}
+          placeholder="url imagen"
+          {...register("image")}
+        />
+        <hr />
 
-        <label htmlFor="">Name</label>
-        <input type="text" {...register("name")} />
+        <Input
+          info={register("name")}
+          error={errors.name?.message}
+          name="name"
+          type={"text"}
+          placeholder="name"
+          {...register("name")}
+        />
+        <hr />
 
-        <label htmlFor="">Model</label>
-        <input type="text" {...register("model")} />
+        <Input
+          info={register("model")}
+          error={errors.model?.message}
+          name="model"
+          type={"text"}
+          placeholder="model"
+          {...register("model")}
+        />
+        <hr />
 
-        <label htmlFor="">Family</label>
-        <input type="text" {...register("family")} />
+        <Input
+          info={register("family")}
+          error={errors.family?.message}
+          name="family"
+          type={"text"}
+          placeholder="family"
+          {...register("family")}
+        />
+        <hr />
 
-        <label htmlFor="">Brand</label>
-        <input type="text" {...register("brand")} />
+        <Input
+          info={register("brand")}
+          error={errors.brand?.message}
+          name="brand"
+          type={"text"}
+          placeholder="brand"
+          {...register("brand")}
+        />
+        <hr />
 
-        <label htmlFor="">Stock</label>
-        <input type="number" {...register("stock")} />
+        <Input
+          info={register("description")}
+          error={errors.description?.message}
+          name="description"
+          type={"text"}
+          placeholder="description"
+          {...register("description")}
+        />
+        <hr />
 
-        <label htmlFor="">Price</label>
-        <input type="number" {...register("price")} />
+        <Input
+          info={register("price")}
+          error={errors.price?.message}
+          name="price"
+          type={"number"}
+          placeholder="price"
+          {...register("price")}
+        />
+        <hr />
 
         <button onClick={handleSubmit(onSubmit)}>Submit</button>
       </form>
