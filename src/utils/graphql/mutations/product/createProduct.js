3251610e1d167651b66477a939194398e1aea2b3
CSRuggeri <santiruu@gmail.com>
2024-01-05 17:27:35 -0300

    se arregla el commit roto

--- src/utils/graphql/mutations/product/createProduct.js
+++ src/utils/graphql/mutations/product/createProduct.js
@@ -1,22 +1,35 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_PRODUCT = gql`
-    mutation CreateProduct(
-        name: String!,
-        model: String!,
-        family: String!,
-        brand: String!,
-        stock: Int!,
-        price: Int!,
-        image: String!
+  mutation CreateProduct(
+    $name: String!
+    $model: String!
+    $family: String!
+    $brand: String!
+    $stock: Int!
+    $price: Int!
+    $image: String!
+  ) {
+    createProduct(
+      input: {
+        name: $name
+        model: $model
+        family: $family
+        brand: $brand
+        stock: $stock
+        price: $price
+        image: $image
+      }
     ) {
-        createProduct(
-            name
-            model
-            family
-            brand
-            stock
-            price
-            image
-        )
-    }`;
+     
+      id
+      name
+      model
+      family
+      brand
+      stock
+      price
+      image
+    }
+  }
+`;
