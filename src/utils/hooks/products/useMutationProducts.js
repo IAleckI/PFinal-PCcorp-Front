726d09fbca1eb00a31710953152c64f13fe17ef1
CSRuggeri <santiruu@gmail.com>
2024-01-06 13:16:05 -0300

    issue correction

--- src/utils/hooks/products/useMutationProducts.js
+++ src/utils/hooks/products/useMutationProducts.js
@@ -2,7 +2,7 @@ import { useMutation, useQuery } from '@apollo/client'
 import { ADD_PRODUCT_TO_CART } from '../../graphql/mutations/products/addProduct'
 import { GET_ALL_USER_PRODUCTS, GET_TOTAL_PRICE } from '../../graphql/querys/products/getAllUserProducts'
 import { DECREASE_PRODUCT } from '../../graphql/mutations/products/decreaseProduct'
-import { jwtDecode } from 'jwt-decode'
+import {jwtDecode} from 'jwt-decode';
 import { useState } from 'react'
 import { useEffect } from 'react'
 import { useDispatch, useSelector  } from 'react-redux'
@@ -10,35 +10,51 @@ import { getProductsRequest, getProducts } from '../../state/features/products/p
 
 
 export const useAddProductToCart = (id) => {
-    const email = jwtDecode(localStorage.getItem('USER_INFO')).email
-    const [addLoading, setLoading] = useState(false)
-    const [addProduct] = useMutation(ADD_PRODUCT_TO_CART)
-    const getPrice = useQuery(GET_TOTAL_PRICE, {
-        variables: { userId: email }
-    })
-    const getProducts = useQuery(GET_ALL_USER_PRODUCTS, {
-        variables: { userId: email }
-    })
-    
-    const addProductToCart  = async () => {
-        setLoading(true)
-        try {
-            await addProduct({
-                variables: {
-                    userId: email,
-                    addUserProductId: id
-                }
-            })
-            const product = await getProducts.refetch()
-            const price = await getPrice.refetch()
-            if (!product.loading && !price.loading) setLoading(false)
-        } catch (error) {
-            console.log(error);
-        }
+    let email = '';
+  try {
+    const userInfo = localStorage.getItem('USER_INFO');
+    if (userInfo) {
+      const decodedToken = jwtDecode(userInfo);
+      email = decodedToken.email;
+    } else {
+      console.warn("User is not logged in. USER_INFO not found in localStorage.");
+      
+      email = ''; 
     }
+  } catch (error) {
+    console.error("Error decoding USER_INFO:", error);
 
-    return { addProductToCart, addLoading }
-}
+    email = ''; 
+  }
+
+  const [addLoading, setLoading] = useState(false);
+  const [addProduct] = useMutation(ADD_PRODUCT_TO_CART);
+  const getPrice = useQuery(GET_TOTAL_PRICE, {
+    variables: { userId: email }
+  });
+  const getProductsQuery = useQuery(GET_ALL_USER_PRODUCTS, {
+    variables: { userId: email }
+  });
+
+  const addProductToCart = async () => {
+    setLoading(true);
+    try {
+      await addProduct({
+        variables: {
+          userId: email,
+          addUserProductId: id
+        }
+      });
+      const product = await getProductsQuery.refetch();
+      const price = await getPrice.refetch();
+      if (!product.loading && !price.loading) setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return { addProductToCart, addLoading };
+};
 
 
 export const useDecreaseProduct = (id) => {
