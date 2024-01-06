45d45d94380d8d1f0033f0ece0b60c43f399e37a
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-01-06 18:45:05 -0300

    Update wishlist.jsx

--- src/Views/Whishlist/wishlist.jsx
+++ src/Views/Whishlist/wishlist.jsx
@@ -8,9 +8,28 @@ const Wishlist = ({ userId }) => {
   const { loading, error, data } = useQuery(GET_ALL_FAVS, {
     variables: { userId: "tuUserId" },
   });
+  
+  if (loading) {
+    return (
+      <div>
+        <NavBar/>
+        <h1>Wishlist</h1>
+        <p>Cargando favoritos...</p>
+        <Footer/>
+      </div>
+    );
+  }
 
-  if (loading) return <p>Cargando favoritos...</p>;
-  if (error) return <p>Error al cargar favoritos: {error.message}</p>;
+  if (error) {
+    return (
+      <div>
+        <NavBar/>
+        <h1>Wishlist</h1>
+        <p>Error al cargar favoritos: {error.message}</p>
+        <Footer/>
+      </div>
+    );
+  }
 
   const favs = data.getAllFavs;
 
