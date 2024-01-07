1ea70d72b428b3a824a6e15cfba203dae831c77d
IAleckI <healsava12@gmail.com>
2024-01-07 12:11:46 -0500

    cambio de footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -5,43 +5,55 @@ import logoGithub from "../../Assets/Logos/logo-github.png";
 
 const Footer = () => {
   return (
-    <hr className={styles.separator} />,
-    <footer className={styles.footer}>
-
-      <div className={styles.socialIcons}>
-        <p className={styles.clickableText}>Repositorio completo de la pagina web en: </p>
-        <a
-          href="https://github.com/IAleckI/PFinal-PCcorp-Front"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={logoGithub} alt="Github" className={styles.githubIcon} />
-        </a>
-      </div>
-      <hr className={styles.separator} />
-
-      <div className={styles.columnContainer}>
-        <div className={styles.column}>
-          <a href="#" className={styles.clickableText}>
-            Enlace 1
+    (<hr className={styles.separator} />),
+    (
+      <footer className={styles.footer}>
+        <div className={styles.socialIcons}>
+          <p className={styles.clickableText}>
+            Repositorio completo de la aplicacion en:{" "}
+          </p>
+          <a
+            href="https://github.com/IAleckI/PFinal-PCcorp-Front"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={logoGithub} alt="Github" className={styles.githubIcon} />
           </a>
         </div>
+        <hr className={styles.separator} />
 
-        <div className={styles.column}>
-          <a href="#" className={styles.clickableText}>
-            Enlace 2
-          </a>
+        <div className={styles.columnContainer}>
+          <div className={styles.column}>
+            <a
+              href="https://tycpcorp.blogspot.com/2024/01/terminos-y-condiciones-de-pcorp.html"
+              target="_blank"
+              className={styles.clickableText}
+            >
+              TYC
+            </a>
+          </div>
+
+          <div className={styles.column}>
+            <a href="/AboutUs" className={styles.clickableText}>
+              About us
+            </a>
+          </div>
+
+          <div className={styles.column}>
+            <a
+              href="https://forms.gle/pnV8ndPC7eAZc3MfA"
+              target="_blank"
+              className={styles.clickableText}
+            >
+              Reclama Aqui
+            </a>
+          </div>
         </div>
-
-        <div className={styles.column}>
-          <a href="#" className={styles.clickableText}>
-            Enlace 3
-          </a>
-        </div>
-
-      </div>
-      <p className={styles.copyrightText}>© {new Date().getFullYear()} All rights reserved with 💙 by PCorp.</p>
-    </footer>
+        <p className={styles.copyrightText}>
+          © {new Date().getFullYear()} All rights reserved with 💙 by PCorp.
+        </p>
+      </footer>
+    )
   );
 };
 
