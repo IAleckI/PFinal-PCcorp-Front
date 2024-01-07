d2eab47e4446f0256ccd6296f1c3caa05554bc62
IAleckI <healsava12@gmail.com>
2024-01-07 12:21:02 -0500

    .

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
           </div>
 
           <div className={styles.column}>
-            <a href="/AboutUs" className={styles.clickableText}>
+            <a href="AboutUs" className={styles.clickableText}>
               About us
             </a>
           </div>
