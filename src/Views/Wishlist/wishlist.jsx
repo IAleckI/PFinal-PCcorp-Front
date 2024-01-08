52afaab325a2774059cdef35fa08447014ba5fbe
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-01-08 00:29:33 -0300

    Wishlist update

--- src/Views/Wishlist/wishlist.jsx
+++ src/Views/Wishlist/wishlist.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "../../components/card/card";
-import { NavBar, Footer } from '../../components/Index'
+import { NavBar, Footer } from "../../components/Index";
 import { useQuery } from "@apollo/client";
 import { GET_ALL_FAVS } from "../../utils/graphql/querys/products/favs/getAllFavs";
 
@@ -10,14 +10,14 @@ const Wishlist = () => {
   const { loading, error, data } = useQuery(GET_ALL_FAVS, {
     variables: { userId: hardcodedUserId },
   });
-  
+
   if (loading) {
     return (
       <div>
-        <NavBar/>
+        <NavBar />
         <h1>Wishlist</h1>
         <p>Cargando favoritos...</p>
-        <Footer/>
+        <Footer />
       </div>
     );
   }
@@ -25,28 +25,35 @@ const Wishlist = () => {
   if (error) {
     return (
       <div>
-        <NavBar/>
+        <NavBar />
         <h1>Wishlist</h1>
         <p>Error al cargar favoritos: {error.message}</p>
-        <Footer/>
+        <Footer />
       </div>
     );
   }
 
   const favs = data.getAllFavs;
 
+  if (favs.length === 0) {
+    return (
+      <div>
+        <NavBar />
+        <h1>Wishlist</h1>
+        <p>No hay productos en la lista de deseos.</p>
+        <Footer />
+      </div>
+    );
+  }
+
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <h1>Wishlist</h1>
       {favs.map((product) => (
-        <Card
-          key={product.id}
-          props={product}
-          isFav={true}
-        />
+        <Card key={product.id} product={product} isFav={true} />
       ))}
-      <Footer/>
+      <Footer />
     </div>
   );
 };
