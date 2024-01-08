07a002530b7c873397da9d07a954c43cf785ec52
CSRuggeri <santiruu@gmail.com>
2024-01-08 01:33:00 -0300

    update on add and del mutations

--- src/utils/graphql/mutations/products/decreaseProduct.js
+++ src/utils/graphql/mutations/products/decreaseProduct.js
@@ -4,6 +4,7 @@ export const DECREASE_PRODUCT = gql`
  mutation($userId: String!, $deleteUserProductId: ID!) {
   deleteUserProduct(userId: $userId, id: $deleteUserProductId) {
     id
+    amount
   }
 }
 `
