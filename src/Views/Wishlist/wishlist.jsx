cef564e09aec983068e393bfac4ab1c5d8beb512
RHolguinCaruso <robert23.holguin06@gmail.com>
2024-01-08 20:31:44 -0500

    Update Wishlist, Cart
    Se agregan condicionales para que tanto el carrito como la wishlist puedan detectar cuando no hay productos, falta diseño

--- src/Views/Wishlist/wishlist.jsx
+++ src/Views/Wishlist/wishlist.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Card from "../../components/card/card";
-import { NavBar, Footer } from '../../components/Index'
+import { NavBar, Footer } from "../../components/Index";
 import { useQuery, useMutation } from "@apollo/client";
 import { GET_ALL_FAVS } from "../../utils/graphql/querys/products/favs/getAllFavs";
 import { DELETE_FAV } from "../../utils/graphql/querys/products/favs/deleteFav";
-import Style from "./wishlist.module.css"
+import Style from "./wishlist.module.css";
+import { Link } from "react-router-dom";
 
 const Wishlist = () => {
   const hardcodedUserId = "pepona@pepona.com";
@@ -14,13 +15,17 @@ const Wishlist = () => {
   });
 
   const [deleteFavMutation] = useMutation(DELETE_FAV, {
-    refetchQueries: [{ query: GET_ALL_FAVS, variables: { userId: hardcodedUserId } }],
+    refetchQueries: [
+      { query: GET_ALL_FAVS, variables: { userId: hardcodedUserId } },
+    ],
   });
 
   const handleDelete = async (productId) => {
     try {
       // Lógica para eliminar el producto de la lista de deseos
-      await deleteFavMutation({ variables: { userId: hardcodedUserId, productId } });
+      await deleteFavMutation({
+        variables: { userId: hardcodedUserId, productId },
+      });
       // Luego, refetch para actualizar la lista después de eliminar
       await refetch();
     } catch (error) {
@@ -31,41 +36,56 @@ const Wishlist = () => {
   if (loading) {
     return (
       <div>
-        <NavBar/>
+        <NavBar />
         <p>Cargando favoritos...</p>
-        <Footer/>
+        <Footer />
       </div>
     );
   }
+  const favs = data.getAllFavs;
 
   if (error) {
     return (
       <div>
-        <NavBar/>
+        <NavBar />
         <p>Error al cargar favoritos: {error.message}</p>
-        <Footer/>
+        <Footer />
       </div>
     );
   }
 
-  const favs = data.getAllFavs;
+  if (!favs || favs.length === 0) {
+    return (
+      <div>
+        <NavBar />
+        <p className={Style.noFavs}>
+          Vaya, parece que aun no tienes favoritos, vuelve al catalogo para
+          agregarlos.
+        </p>
+        <Link to="/catalogo">
+          <button className={Style.noFavsButton}>Volver al catalogo</button>
+        </Link>
+        <Footer />
+      </div>
+    );
+  }
 
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <div className={Style.cardContainer}>
         {favs.map((product) => (
           <Card
             key={product.id}
             props={product}
-            isWishlist={true} 
+            isWishlist={true}
             onDelete={handleDelete}
           />
         ))}
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
-}
+};
 
 export default Wishlist;
--- src/components/cartList/cartList.jsx
+++ src/components/cartList/cartList.jsx
@@ -1,24 +1,36 @@
-import Style from './cartList.module.css'
-import CheckOut from '../checkout/checkout'
-import CardCard from '../cartCards/cardCard'
-import { useGetProducts } from '../../utils/hooks/products/useMutationProducts'
+import Style from "./cartList.module.css";
+import CheckOut from "../checkout/checkout";
+import CardCard from "../cartCards/cardCard";
+import { useGetProducts } from "../../utils/hooks/products/useMutationProducts";
+import { Link } from "react-router-dom";
 
 export default function CartList() {
-    const { products, loading, error } = useGetProducts()
-    console.log("products:",products)
-    
-    if (loading ) return <p>Cargando...</p>
-    if (error) return <p>Error: {error.message}</p>    
+  const { products, loading, error } = useGetProducts();
+  console.log("products:", products);
 
+  if (loading) return <p>Cargando...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  if (!products || products.length === 0) {
     return (
-        <div className={Style.cartList_container}>
-          <div className={Style.cartList}>
-          <h1>Carrito</h1>
-            {products.map(p => (
-                <CardCard key={p.id} props={p}/>
-            ))}
-          </div>
-          <CheckOut/>
-        </div>
-    )
+      <div>
+        <p className={Style.noProducts}>Aún no agregas productos a tu carrito, entra al catalogo para agregarlos</p>
+        <Link to="/catalogo">
+          <button>Ir a la tienda</button>
+        </Link>
+      </div>
+    );
+  }
+
+  return (
+    <div className={Style.cartList_container}>
+      <div className={Style.cartList}>
+        <h1>Carrito</h1>
+        {products.map((p) => (
+          <CardCard key={p.id} props={p} />
+        ))}
+      </div>
+      <CheckOut />
+    </div>
+  );
 }
