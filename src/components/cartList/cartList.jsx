ecf7213e91a8e698532eafbda7ea14aaf7a0002e
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-01-10 13:25:39 -0300

    cart y checkout

--- src/components/cartList/cartList.jsx
+++ src/components/cartList/cartList.jsx
@@ -4,21 +4,21 @@ import CardCard from '../cartCards/cardCard'
 import { useGetProducts } from '../../utils/hooks/products/useMutationProducts'
 
 export default function CartList() {
-    const { products, loading, error } = useGetProducts()
-    console.log("products:",products)
-    
-    if (loading ) return <p>Cargando...</p>
-    if (error) return <p>Error: {error.message}</p>    
+  const { products, loading, error } = useGetProducts();
+  console.log("products:", products);
 
-    return (
-        <div className={Style.cartList_container}>
+  if (loading) return <p>Cargando...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+      <div className={Style.cartList_container}>
           <div className={Style.cartList}>
-          <h1>Carrito</h1>
-            {products.map(p => (
-                <CardCard key={p.id} props={p}/>
-            ))}
+              <h1>Carrito</h1>
+              {products.map(p => (
+                  <CardCard key={p.id} props={p} />
+              ))}
           </div>
-          <CheckOut/>
-        </div>
-    )
+          <CheckOut cartProducts={products} />
+      </div>
+  );
 }
