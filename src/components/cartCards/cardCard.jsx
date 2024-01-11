5045fc563735596b7d57cdbadc4a5b1c7ce2b6ca
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-01-11 13:27:58 -0300

    Css y card tolocalestring

--- src/components/cartCards/cardCard.jsx
+++ src/components/cartCards/cardCard.jsx
@@ -6,7 +6,7 @@ import { useDecreaseProduct } from '../../utils/hooks/products/useMutationProduc
 export default function CardCard(props) {
     const { id, name, image, total, amount} = props.props
     const { addProductToCart } = useAddProductToCart(id)
-    const { decreaseProductoOfCart, loading } = useDecreaseProduct(id)
+    const { decreaseProductoOfCart } = useDecreaseProduct(id)
 
     return (
         <div className={Style.card}>
@@ -15,7 +15,7 @@ export default function CardCard(props) {
               <h2>{name}</h2>
             </div>
             <div className={Style.card_data}>
-              <h3>{total}</h3>
+            <h3 className={Style.preciocart}> ${total.toLocaleString('es-ES', { maximumFractionDigits: 0 })}</h3>
               <div className={Style.card_amount}>
                 <button onClick={decreaseProductoOfCart} >-</button>
                 <h3>{amount}</h3>
