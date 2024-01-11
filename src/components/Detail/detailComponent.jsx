862eb592add13ab94813062013347d17a84d614e
TSantiagoIdalgo <tomasidalgo51@gmail.com>
2024-01-10 23:45:41 -0300

    feat(detail payment)

--- src/components/Detail/detailComponent.jsx
+++ src/components/Detail/detailComponent.jsx
@@ -3,12 +3,14 @@ import { useParams } from "react-router-dom";
 import { Button } from "../Index";
 import { GET_PRODUCT_BY_ID } from "../../utils/graphql/querys/products/getProductById";
 import Style from "./detailComponent.module.css";
+import { useAddProductToCart } from "../../utils/hooks/products/useMutationProducts";
 
 const ProductDetail = () => {
   const { id } = useParams();
   const { loading, error, data } = useQuery(GET_PRODUCT_BY_ID, {
     variables: { id },
   });
+  const { addProductToCart } = useAddProductToCart(id)
 
   if (loading) return <h1 className={Style.loading}>Cargando...</h1>;
   if (error)
@@ -36,7 +38,7 @@ const ProductDetail = () => {
         
         <Button 
           text={'Añadir al carrito'}
-          onClick={() => console.log('añadido al carrito')}
+          onClick={addProductToCart}
           />
       </div>
     </div>
