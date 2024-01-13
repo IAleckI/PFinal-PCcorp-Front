c1b4b219e103769c4f85c0e2ec97fd6377656e1d
Santiago Scheuermann <santisch1997@gmail.com>
2024-01-13 14:22:34 -0300

    Card Detail

--- src/components/Detail/detailComponent.jsx
+++ src/components/Detail/detailComponent.jsx
@@ -44,7 +44,7 @@ const ProductDetail = () => {
             <h2>Tipo: {getProductById.type}</h2>
             <h2 className={Style.Stock}>Stock: {getProductById.stock}</h2>
           </div>
-          <Button text={"Añadir al carrito"} onClick={addProductToCart} />
+          <Button className= {Style.ButtonDetail} text={"Añadir al carrito"} onClick={addProductToCart} />
         </div>
       </div>
 
