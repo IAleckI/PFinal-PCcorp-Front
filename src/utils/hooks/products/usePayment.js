a590ce68229bd9448764fff018f9ef062d2ec152
TSantiagoIdalgo <130645404+TSantiagoIdalgo@users.noreply.github.com>
2024-01-13 17:25:54 -0300

    Update usePayment.js

--- src/utils/hooks/products/usePayment.js
+++ src/utils/hooks/products/usePayment.js
@@ -34,7 +34,9 @@ export const usePayment = () => {
                 })
                 
                 const paymentResult = await getPayment({
-                    variables: { items: mapped }, 
+                    variables: { 
+                        userId: email,
+                        items: mapped }, 
                 })
 
                 setPaymentId(paymentResult.data.createPayment)
