7e0e24119a87bac76def23dcb88687cad9f43a6c
Santiago Scheuermann <santisch1997@gmail.com>
2024-01-16 15:34:18 -0300

    g

--- src/components/AboutUs/aboutUs.jsx
+++ src/components/AboutUs/aboutUs.jsx
@@ -7,6 +7,7 @@ import partnerIcon from '../../Assets/Img/AboutUsImgs/partner1.png';
 import futureIcon from '../../Assets/Img/AboutUsImgs/future.png';
 import photoExample from '../../Assets/Img/AboutUsImgs/fotoejemplo.jpg';
 import santisch from '../../Assets/Img/AboutUsImgs/santisch.png';
+import guille from '../../Assets/Img/AboutUsImgs/guilleB.jpg';
 
 
 const AboutUsComponent = () => {
@@ -18,7 +19,7 @@ const AboutUsComponent = () => {
     { name: 'Robert H.', photo: photoExample },
     { name: 'Valentin S.', photo: photoExample },
     { name: 'Santiago S.', photo: santisch },
-    { name: 'Guillermo B.', photo: photoExample },
+    { name: 'Guillermo B.', photo: guille },
   ];
 
   return (
