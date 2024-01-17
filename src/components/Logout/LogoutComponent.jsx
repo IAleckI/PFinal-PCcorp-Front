2e82e46dc6e45bb6c1cad97872a6c47591b05721
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-01-17 13:50:09 -0300

    Update LogoutComponent.jsx

--- src/components/Logout/LogoutComponent.jsx
+++ src/components/Logout/LogoutComponent.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import swal from "sweetalert";
 import Style from "./Logout.module.css";
-import {Button} from "../Index";
+import { Button } from "../Index";
 
 const LogoutButton = () => {
   let user = JSON.parse(localStorage.getItem("USER-INFO"));
   const navigate = useNavigate();
+
   const logOut = () => {
     swal({
       title: "Logout!",
-      text: "Has cerrado sesion",
+      text: "Has cerrado sesión",
       icon: "success",
       button: "Volver",
-    })
-    localStorage.clear(user);
-<<<<<<< Updated upstream
-    navigate("/");
-=======
-    navigate("/login");
+    });
 
-    window.location.reload();
->>>>>>> Stashed changes
+    localStorage.clear(user);
+    
+
+    setTimeout(() => {
+      navigate("/login");
+
+
+      window.location.reload();
+    }, 1500);
   };
-  return (
-    <Button onClick={logOut} text={"Logout"}  />
- 
-  );
+
+  return <Button onClick={logOut} text={"Logout"} />;
 };
 
 export default LogoutButton;
