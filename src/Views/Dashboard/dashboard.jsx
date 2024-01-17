06fd1e3e2f147f0190767eb4933c463d36ffd096
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-01-16 23:48:31 -0300

    .

--- src/Views/Dashboard/dashboard.jsx
+++ src/Views/Dashboard/dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {NavBar, Footer} from "../../components/Index"
 import {Update, DashboardAside, UsersDash} from "../../components/Index"
+import Style from './dashboard.module.css'
 
 
 export const Dashboard = () => {
@@ -9,15 +10,21 @@ export const Dashboard = () => {
     <div>
         <NavBar/>
         <div>
-        <h2>Products Board:</h2>
-        <DashboardAside/>
+          <h2 className={Style.productboard}>Products Board</h2>
+            <h3 className={Style.dashboardaside}>
+            <DashboardAside/> 
+            </h3>
+            <div className={Style.update}>
          <Update/>
          </div>
-         <div>
-          <h2>Users Board:</h2>
-<UsersDash/>
+        </div>
+          <div>
+            <h2 className={Style.userboard}>Users Board</h2>
+            <div className={Style.userdash}>
+        <UsersDash/>
+        </div>
          </div>
         <Footer/>
-        </div>
+    </div>
   )
 }
